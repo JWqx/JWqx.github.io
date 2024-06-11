59e2973023e3e38101be2f32ccc8df82717528bf
Jay <wqxjay@163.com>
2024-06-11 17:54:33 +0800

    Site updated: 2024-06-11 17:54:33

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/06/11/DIP流程/","2024/06/11/SMT贴片工艺/","2024/06/11/光伏系统/","2024/06/11/hello-world/"];function toRandomPost(){
+var posts=["2024/06/11/SMT贴片工艺/","2024/06/11/hello-world/","2024/06/11/DIP流程/","2024/06/11/光伏系统/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
